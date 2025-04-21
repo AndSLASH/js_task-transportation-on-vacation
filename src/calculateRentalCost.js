65928f543024309539c4de34edf3116fbccc37ad
Andrii Zakharov <zaharov.andrey.93@gmail.com>
2025-04-21 12:40:41 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const basePrice = 40;
+  const discountMin = 20;
+  const discountMax = 50;
+
+  const minDays = 3;
+  const maxDays = 7;
+
+  const totalCost = basePrice * days;
+
+  let discountPrice = 0;
+
+  if (days >= minDays && days < maxDays) {
+    discountPrice = totalCost - discountMin;
+  } else if (days >= maxDays) {
+    discountPrice = totalCost - discountMax;
+  } else {
+    discountPrice = totalCost;
+  }
+
+  return discountPrice;
 }
 
 module.exports = calculateRentalCost;
